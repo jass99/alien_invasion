6115327e1ca4484fd33ac4291c702675e50c411b
jass99 <30364213+jass99@users.noreply.github.com>
2023-05-06 18:02:16 -0400

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,18 @@
+class Settings():
+    
+    def __init__(self):
+        self.screen_width = 1100
+        self.screen_height = 700
+        self.bg_color = (0,0,0)
+        
+        self.ship_speed_factor = 0.5
+        
+        # Bullets settings
+        
+        self.bullet_speed_factor = 0.3
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 255,255,255
+        
+        
+        
